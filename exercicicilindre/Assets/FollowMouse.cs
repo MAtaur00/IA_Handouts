a9d0cec4746ebe58b9e414a036d34c84aba682bb
MAtaur00 <mtmtarres@gmail.com>
2019-09-26 21:44:24 +0200

    cilindre

---
+++ exercicicilindre/Assets/FollowMouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour {
+    public float speed = 5.0f;
+
+    public GameObject objective;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, objective.transform.position, speed*Time.deltaTime);
+        transform.LookAt(objective.transform.position, Vector3.right);
+	}
+}
