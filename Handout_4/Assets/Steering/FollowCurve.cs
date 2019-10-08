42176e69e96495330ae77c3145cca5cfaec3bb23
MAtaur00 <mtmtarres@gmail.com>
2019-10-08 09:21:26 +0200

    handout 4 path (todo 1 done)

---
+++ Handout_4/Assets/Steering/FollowCurve.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BansheeGz.BGSpline.Components;
+using BansheeGz.BGSpline.Curve;
+
+public class FollowCurve : MonoBehaviour
+{
+    public BansheeGz.BGSpline.Components.BGCcMath path;
+    float ratio = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ratio += 0.01f;
+        if (ratio > 1.0f)
+        {
+            ratio = 0.0f;
+        }
+        transform.position = path.CalcPositionByDistanceRatio(ratio);
+    }
+}
